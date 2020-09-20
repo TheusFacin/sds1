8bb13d6b6e0b83980888c386f13a5b7c65f5b227
Matheus Facin <matheus.facin@outlook.com>
2020-09-20 17:13:20 -0300

    Criando navegação com rotas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
+
+import Routes from './routes';
+
 import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <Routes />
   );
 }
 
