a5f892fc3acc662adff8ff195aebb4ca4f5ba27a
Matheus Facin <matheus.facin@outlook.com>
2020-09-20 18:28:23 -0300

    Criando página de listagem de registros

---
+++ front-web/src/pages/Records/Pagination/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './styles.css'
+
+type Props = {
+  totalPages?: number;
+  goToPage: Function;
+  activePage: number;
+}
+
+const Pagination = ({ totalPages = 0, goToPage, activePage }: Props) => {
+  const paginationItems = Array.from(Array(totalPages).keys());
+
+  return (
+    <div className="pagination-container">
+      <button
+        className={`pagination-item ${activePage > 0 ? 'active': 'inactive'}`}
+        onClick={activePage > 0 ? () => goToPage(activePage - 1) : () => {}}
+      >
+        {'<'}
+      </button>
+
+      {paginationItems.map(item => (
+        <button
+          key={item}
+          className={`pagination-item ${activePage === item ? 'active': 'inactive'}`}
+          onClick={() => goToPage(item)}
+        >
+          {item + 1}
+        </button>
+      ))}
+
+      <button
+        className={`pagination-item ${activePage < totalPages - 1 ? 'active': 'inactive'}`}
+        onClick={activePage < totalPages - 1 ? () => goToPage(activePage + 1) : () => {}}
+      >
+        {'>'}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const Records = () => (
-  <h1>Records</h1>
-);
+import Pagination from './Pagination';
+import { RecordsResponse } from './types';
+import { formatDate } from './helpers';
+
+import './styles.css';
+import { Link } from 'react-router-dom';
+
+const BASE_URL = 'http://localhost:8080';
+
+const Records = () => {
+  const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+  const [activePage, setActivePage] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(
+        `${BASE_URL}/records?linesPerPage=12&page=${activePage}`
+      );
+      setRecordsResponse(response.data);
+    })();
+  }, [activePage]);
+
+  const handlePageChange = (index: number) => {
+    setActivePage(index)
+  }
+
+  return (
+    <div className="page-container">
+      <div className="filters-container records-actions">
+        <Link to="/charts">
+          <button className="action-filters">
+            Ver Gráficos
+          </button>
+        </Link>
+      </div>
+
+      <table className="records-table" cellPadding={0} cellSpacing={0}>
+        <thead>
+          <tr>
+            <th>Instante</th>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Plataforma</th>
+            <th>Gênero</th>
+            <th>Título do Game</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recordsResponse?.content.map((record) => (
+            <tr key={record.id}>
+              <td>{formatDate(record.moment)}</td>
+              <td>{record.name}</td>
+              <td>{record.age}</td>
+              <td className="text-secondary">{record.gamePlatform}</td>
+              <td>{record.genreName}</td>
+              <td className="text-primary">{record.gameTitle}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Pagination totalPages={recordsResponse?.totalPages} goToPage={handlePageChange} activePage={activePage} />
+    </div>
+  );
+};
 
 export default Records;
